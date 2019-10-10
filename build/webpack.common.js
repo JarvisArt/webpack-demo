0abdbafd9a0fa3cc8147ba3dd3856e026d0b7ce4
Jarvis <1120886013@qq.com>
2019-10-10 19:35:59 +0800

    代码分离

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     publicPath: '/',
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist')
   },
   module: {
     rules: [
@@ -64,5 +64,28 @@ module.exports = {
       template: 'src/index.html'
     }),
     new CleanWebpackPlugin(),
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      chunks: "async",
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+        },
+        default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+        }
+      }
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import { add } from './math';
+// import _ from 'loadsh';
 
-add(1, 4);
+// console.log(_.join(['a', 'b', 'c'], '——'));
+// console.log(_.join(['a', 'b', 'c'], '——'));
+
+function getComponent() {
+  return import(/* webpackChunkName:"lodash" */'loadsh').then(({ default: _ }) => {
+    var element = doucment.createElement('div');
+    element.innerHTML = _.join(['Jarvis', 'Art'], '—')
+    return element;
+  })
+}
+
+getComponent().then(element => {
+  doucment.body.appendChild(element);
+})
