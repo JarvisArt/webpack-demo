63418c68caff4923fe2e741e5fbab7c10844152f
Jarvis <1120886013@qq.com>
2019-10-10 16:59:32 +0800

    核心配置

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('./webpack.config');
+const complier = webpack(config);
+
+// 在node中直接使用webpack
+const app = express();
+app.use(webpackDevMiddleware(complier, {
+  publicPath: config.output.publicPath
+}))
+
+app.listen(3000, () => {
+  console.log('server is running')
+})
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development', // development 或 production
+  devtool: 'cheap-module-eval-source-map',
+  entry: {
+    main: './src/index.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    port: 8080,
+    hot: true,
+    hotOnly: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            limit: 2000
+          }
+        }
+      },
+      {
+        test: /\.(eot|ttf|svg|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          'sass-loader',
+          'postcss-loader',
+        ]
+      }
+    ]
+  },
+  output: {
+    publicPath: '/',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  optimization: {
+    usedExports: true
+  }
+}
