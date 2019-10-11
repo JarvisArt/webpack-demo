5de8060be58c0359ac3324e6ae309bbca3f7acd1
Jarvis <1120886013@qq.com>
2019-10-11 19:22:36 +0800

    打包分析，Preloading, Prefetching

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-// import _ from 'loadsh';
-
-// console.log(_.join(['a', 'b', 'c'], '——'));
-// console.log(_.join(['a', 'b', 'c'], '——'));
-
-function getComponent() {
-  return import(/* webpackChunkName:"lodash" */'loadsh').then(({ default: _ }) => {
-    var element = doucment.createElement('div');
-    element.innerHTML = _.join(['Jarvis', 'Art'], '—')
-    return element;
+document.addEventListener('click', () => {
+  import(/* webpackPrefetch: true */ './click.js').then(({ default: func }) => {
+    func();
   })
-}
-
-getComponent().then(element => {
-  doucment.body.appendChild(element);
 })
